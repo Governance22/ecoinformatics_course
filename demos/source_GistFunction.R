32110f65d26e80ef41db2e03dc8e8a093b0de0d0
Lin, Cheng-Tao <mutolisp@gmail.com>
2018-06-17 18:01:58 +0800

    add gist function sharing images

---
+++ demos/source_GistFunction.R
@@ -0,0 +1,4 @@
+library(devtools)
+source_gist('643dc4795d0b31f1ef3fdaca1641fa7e', filename = 'ShannonWiener.r')
+
+ShannonWiener('https://raw.githubusercontent.com/mutolisp/ecoinformatics_course/master/data/twforest.csv')
